a65b41e4b1f280909860ae30ee18aab228590001
anegrete <anegrete@iastate.edu>
2018-12-17 18:10:05 -0600

    initial setup

--- DotNetApiLambda/Startup.cs
+++ DotNetApiLambda/Startup.cs
@@ -26,9 +26,6 @@ namespace DotNetApiLambda
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            // Add S3 to the ASP.NET Core dependency injection framework.
-            services.AddAWSService<Amazon.S3.IAmazonS3>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
